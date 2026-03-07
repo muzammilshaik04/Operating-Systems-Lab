28094a3a5024430b4e3bd8152b0cdd533391b7b3
Muhammed Muzammil Shaik <muhammedmuzammil.cs24@bmsce.ac.in>
2026-03-07 18:12:00 +0530

    Add files via upload

---
+++ Lab-2/FCFS/fcfs.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int pid[n], at[n], bt[n];
+    int ct[n], tat[n], wt[n];
+    int current_time = 0;
+    float avg_tat = 0, avg_wt = 0;
+
+    for(i = 0; i < n; i++) {
+        pid[i] = i + 1;
+        printf("\nProcess %d\n", pid[i]);
+        printf("Enter Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Enter Burst Time: ");
+        scanf("%d", &bt[i]);
+    }
+
+    for(i = 0; i < n-1; i++) {
+        for(j = i+1; j < n; j++) {
+            if(at[i] > at[j]) {
+                int temp;
+                temp = at[i]; at[i] = at[j]; at[j] = temp;
+                temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
+                temp = pid[i]; pid[i] = pid[j]; pid[j] = temp;
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        if(current_time < at[i])
+            current_time = at[i];
+
+        ct[i] = current_time + bt[i];
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+
+        current_time = ct[i];
+
+        avg_tat += tat[i];
+        avg_wt += wt[i];
+    }
+
+    avg_tat /= n;
+    avg_wt /= n;
+
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+
+    for(i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n",
+               pid[i], at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
+
+    printf("\nAverage Turnaround Time = %.2f", avg_tat);
+    printf("\nAverage Waiting Time = %.2f\n", avg_wt);
+
+    return 0;
+}
